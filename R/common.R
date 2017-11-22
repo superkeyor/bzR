582eae277b337cb0f2ee809683d4488c093f2ae6
Jerry Zhu <jerryzhujian9@gmail.com>
2017-11-22 11:34:28 -0600

    update

--- R/common.R
+++ R/common.R
@@ -3,12 +3,13 @@
 ###**************************************************.
 
 #' Install a package from bioconductor
-#' @description internally source/install biocLite.R, wrapper of biocLite(pkg)
+#' @description internally source/install biocLite.R, wrapper of \code{\link[BiocInstaller]{biocLite}}
 #' @param pkg eg, 'biomaRt'
+#' @param suppressUpdates if TRUE, do not ask to update other old packages and do not update them; if FALSE, will ask to choose
 #' @return returns nothing
 #' @export
-bz.install = function(pkg) {
+bz.install = function(pkg,suppressUpdates=TRUE,...) {
     source("https://bioconductor.org/biocLite.R")
-    biocLite(pkg)
+    biocLite(pkg,suppressUpdates=suppressUpdates,...)
 }
 
