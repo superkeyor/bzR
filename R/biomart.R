a12da47684f812f534fcc5d4727c876c51664ae8
Jerry Zhu <jerryzhujian9@gmail.com>
2017-11-22 16:09:53 -0600

    update

--- R/biomart.R
+++ R/biomart.R
@@ -12,7 +12,8 @@
 mart.list <- function(what='snp',host=NULL,...) {
     if (!(what %in% c('hosts', 'marts', 'snp', 'gene'))) {
         ez.print("double check 'what', should be one of 'hosts', 'marts', 'snp', 'gene'")
-        return()
+        # invisible(return())
+        return
     }
 
     if (is.null(host)) {host='www.ensembl.org'}
